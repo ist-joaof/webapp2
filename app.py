c59e9eda67b4ab732af9eb6a0c88c6b9d07e00d6
ist-joaof <joao.fonseca@ist.utl.pt>
2022-07-19 16:59:30 +0100

    Update app.py
    test

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-from flask import request
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
 	out = ''
-	for line in request.headers:
+	for line in flask.Flask.request.headers:
 		out += '<p>' + str(line) + '</p>'
 	return(out)
